ad9c5978b430f4c94c44dcd94a9a517c0501e3aa
Natch Khongpasuk <natch.kh@gmail.com>
2021-02-09 00:18:18 -0500

    services section main page - add inspection and certification cards

--- components/index/services.js
+++ components/index/services.js
@@ -1,12 +1,9 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBug, faLongArrowAltRight } from '@fortawesome/free-solid-svg-icons'
+import { faBug, faLongArrowAltRight, faCertificate, faSearchPlus, faCheckSquare } from '@fortawesome/free-solid-svg-icons'
 
 export default function Services () {
     return (
         <section>
-            <h2>Services</h2>
-            <p>We comply with complex global compliance legislation in accordance with the FDA, EU GMP, Federal Regulations, state regulations, and Health Canada. We develop systems and quality assurance programs to provide an accelerated approach to incorporating (GMPs) good manufacturing practices to fit your specific business needs.</p>
-            
             <div className="services-area grey-soft-bg pt-110 pb-95">
                 <div className="container">
                     <div className="row">
@@ -18,13 +15,15 @@ export default function Services () {
                         </div>
                     </div>
                     <div className="row">
-                        <div className="col">
+                        <div className="col-lg-8 offset-lg-2">
                             <p>
-                                We provide solutions through our testing, inspection and certification services we ensure to businesses, growers, processors, manufacturer, distributors and retailers with navigating legal compliance and opening barriers to global business opportunities.  Through our testing, inspection and certification services we ensure clients meet all legal requirements while maximizing quality assurance and minimizing risk
-                            </p>
+                                We provide solutions through our testing, inspection and certification services we ensure to businesses, growers, processors, manufacturer, distributors and retailers with navigating legal compliance and opening barriers to global business opportunities.  Through our testing, inspection and certification services we ensure clients meet all legal requirements while maximizing quality assurance and minimizing risk.
+                                <br /><br />
+                                We comply with complex global compliance legislation in accordance with the FDA, EU GMP, Federal Regulations, state regulations, and Health Canada. We develop systems and quality assurance programs to provide an accelerated approach to incorporating (GMPs) good manufacturing practices to fit your specific business needs.</p>
                         </div>
                     </div>
                     <div className="row">
+                        { /* Start of TESTING service */ }
                         <div className="col-xl-4 col-lg-6 col-md-6">
                             <div className="services-wrapper pos-rel mb-30 wows fadeInUps animateds" data-wows-delay="0.3s">
                                 <div className="services-icon">
@@ -39,6 +38,39 @@ export default function Services () {
                                 </div>
                             </div>
                         </div>
+                        { /* End of TESTING service */ }
+                        { /* Start of INSPECTION service */ }
+                        <div className="col-xl-4 col-lg-6 col-md-6">
+                            <div className="services-wrapper pos-rel mb-30 wows fadeInUps animateds" data-wows-delay="0.3s">
+                                <div className="services-icon">
+                                    <i><FontAwesomeIcon icon={faSearchPlus} /></i>
+                                </div>
+                                <div className="services-text">
+                                    <h3>Inspection</h3>
+                                    <p>Some inspection text here</p>
+                                </div>
+                                <div className="services-link">
+                                    <a href="#contact"><i><FontAwesomeIcon icon={faLongArrowAltRight} /></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        { /* End of INSPECTION service */ }
+                        { /* Start of CERTIFICATION service */ }
+                        <div className="col-xl-4 col-lg-6 col-md-6">
+                            <div className="services-wrapper pos-rel mb-30 wows fadeInUps animateds" data-wows-delay="0.3s">
+                                <div className="services-icon">
+                                    <i><FontAwesomeIcon icon={faCheckSquare} /></i>
+                                </div>
+                                <div className="services-text">
+                                    <h3>Certification</h3>
+                                    <p>Some certificate text here</p>
+                                </div>
+                                <div className="services-link">
+                                    <a href="#contact"><i><FontAwesomeIcon icon={faLongArrowAltRight} /></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        { /* End of CERTIFICATION service */ }
                     </div>
                 </div>
             </div>
