f5140a212886b5c18d28adcc2fe11069e217c839
Natch Khongpasuk <natch.kh@gmail.com>
2021-02-17 18:58:53 -0500

    revert back to using relative paths import

--- next.config.js
+++ next.config.js
@@ -4,12 +4,12 @@ module.exports = {
         return {
             '/': { page: '/' },
             '/services': { page: '/services' },
-            '/services/microbiology-screening': { page: 'services/microbiology-screening' },
+            /* '/services/microbiology-screening': { page: 'services/microbiology-screening' },
             '/services/pesticides-screening': { page: 'services/pesticides-screening' },
             '/services/hemp-products-test': { page: 'services/hemp-products-test' },
             '/services/hemp-flower-potency': { page: 'services/hemp-flower-potency' },
             '/services/total-cannabis-cannabinoid-profile': { page: 'services/total-cannabis-cannabinoid-profile' },
-            '/services/aflatoxin': { page: 'services/aflatoxin' }
+            '/services/aflatoxin': { page: 'services/aflatoxin' } */
         }
     },
     images: {
