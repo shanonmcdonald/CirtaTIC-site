a61c8d6df2b541d8399a9c57d57f520cfc30c7a8
Natch Khongpasuk <natch.kh@gmail.com>
2021-02-23 17:31:47 -0500

    services page update and remove yml logout

--- pages/services.js
+++ pages/services.js
@@ -13,7 +13,7 @@ const Services = () => {
         <Header />
         
         <main className="services">
-          <h1>Services</h1>
+          <h1>Services Page</h1>
         </main>
 
         <Footer />
