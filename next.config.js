8a402d2ce3d5e0fca46cded490a8dc70b501293a
Natch Khongpasuk <natch.kh@gmail.com>
2021-04-03 01:43:40 -0400

    Add new pages to config file

--- next.config.js
+++ next.config.js
@@ -3,8 +3,10 @@ module.exports = {
     exportPathMap: function() {
         return {
             '/': { page: '/' },
+            '/contact': { page: '/contact' },
             '/services': { page: '/services' },
             '/gmp-mandatory-testing': { page: '/gmp-mandatory-testing' },
+            '/soil-and-water-testing': { page: '/soil-and-water-testing' },
         }
     },
     images: {
