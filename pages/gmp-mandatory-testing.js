0a32d0053b9731a318a2f04a55d1b8fbf4e30d3b
Natch Khongpasuk <natch.kh@gmail.com>
2021-04-20 17:59:12 -0400

    expand read more - gcmp page

--- pages/gmp-mandatory-testing.js
+++ pages/gmp-mandatory-testing.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react'
 import Head from 'next/head'
 
 import Header from '../components/index/header'
@@ -17,7 +18,7 @@ const testingServices = [
         Salmonella spp. = <USP> ch. 2022
         Enterbacterial count = <USP> ch. 2021
         S. aureus = <USP> ch. 2022`,
-        //description:"",
+        read: 'Let\'s go to a little walk and shit baby',
         icon: faBug,
         bg: ['/microbiology_bg.jpg']
     }, {
@@ -30,20 +31,21 @@ const testingServices = [
         Methodology: GMP cannabis mandatory release cannabis
         pesticides limits list`,
         icon: faTimes,
-        bg: ['/pesticide_bg.jpg']
+        bg: ['/pesticide_bg.jpg'],
+        read: 'Let\'s go to a little walk and shit baby',
     }, {
         title: 'Hemp flower compliance test',
         link: 'hemp-flower-potency',
         //description: 'Test for compliance DL=< 0.3%',
         description:`
-        Hemp plant total THC potency test for compliance DL =<
-        0.3%
+        Hemp plant total THC potency test for compliance DL =< 0.3%
         Organisation: Association of Official Agricultural Chemists
         (International)
         Methodology: AFVAN-SLMF-0029
         Instrumentation: Gas Chromatography (GC)`,
         icon: faCertificate,
-        bg: ['/gc_bg.jpg']
+        bg: ['/gc_bg.jpg'],
+        read: 'Let\'s go to a little walk and shit baby',
     }, {
         title: 'Aflatoxin',
         link: 'aflatoxin',
@@ -56,14 +58,14 @@ const testingServices = [
         Instrumentation: Liquid Chromatography + Mass
         spectrometry`,
         icon: faExclamationTriangle,
-        bg: ['/lc_bg.jpg', '/ms_logo.png']
+        bg: ['/lc_bg.jpg', '/ms_logo.png'],
+        read: 'Let\'s go to a little walk and shit baby',
     }, {
         title: 'Hemp products compliance test',
         link: 'hemp-products-test',
         //description: 'Total THC potency test DL =< 4ppm',
         description:`
-        Hemp products total THC potency test for compliance DL =
-        4ppm
+        Hemp products total THC potency test for compliance DL = 4ppm
         Organisation: Association of Official Agricultural Chemists
         (International)
         Methodology = AFVAN-SMF-0027
@@ -71,7 +73,8 @@ const testingServices = [
         spectrometry
         `,
         icon: faPlusSquare,
-        bg: ['/gc_bg.jpg', '/ms_logo.png'] // TODO SHANON: put image name here
+        bg: ['/gc_bg.jpg', '/ms_logo.png'],
+        read: 'Let\'s go to a little walk and shit baby',
     }, {
         title: 'Total Cannabis Cannabinoid Profile',
         link: 'total-cannabis-cannabinoid-profile',
@@ -85,9 +88,8 @@ const testingServices = [
         Methodology: SGS proprietary method
         Instrumentation: Liquid Chromatography
         `,
-
-        icon: faFlask,
-        bg: ['/canna-anal_bg.jpg','/cannaresults_logo.png'] // TODO SHANON: put image name here
+        bg: ['/canna-anal_bg.jpg','/cannaresults_logo.png'],
+        read: 'Let\'s go to a little walk and shit baby',
     },
     {
         title: 'Heavy metals',
@@ -101,8 +103,8 @@ const testingServices = [
         Instrumentation: Inductively coupled plasma mass
         spectrometry
         `,
-        bg: ['/plasma_bg.jpg'] // TODO SHANON: put image name here
-        //icon: faFlask
+        bg: ['/plasma_bg.jpg'],
+        read: 'Let\'s go to a little walk and shit baby',
     },
     {
         title: 'Moisture content',
@@ -113,39 +115,52 @@ const testingServices = [
         (International)
         Methodology: AFVAN-SLMF-0013
         Instrumentation: Scientific air oven`,
-        bg: ['/airoven_bg.jpg'] // TODO SHANON: put image name here
-        //icon: faExclamationTriangle
+        bg: ['/airoven_bg.jpg'],
+        read: 'Let\'s go to a little walk and shit baby haha school teaccher baby hoe hoe',
     }
 ]
 
-const Services = () => {
-    return testingServices.map(service => {
-        return (
-            <div className="container mx60" key={service.title}>
-                <div className="row">
-                    <div className="col">
-                            <h3 style={{'margin': '50px 0'}} key={service.link} id={service.link}>{service.title}</h3>
+function ServiceElement ({ service }) {
+    const [isOpen, setIsOpen] = useState(false)
+
+    function handleSetIsOpen (e) {
+        e.preventDefault()
+        setIsOpen(true)
+    }
+
+    return (
+        <div className="container mx60">
+            <div className="row">
+                <div className="col">
+                    <h3 style={{'margin': '50px 0'}} key={service.link} id={service.link}>{service.title}</h3>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-md-5 offset-md-1">
+                    <p className="service--description">{service.description}</p>
+                    <div className="lb-btn mt-35">
+                        { !isOpen && <a className="service__readmore" href="#" onClick={handleSetIsOpen} title="Click to read more">Read More</a> }
                     </div>
                 </div>
-                <div className="row">
-                    <div className="col-md-5 offset-md-1">
-                        <p className="service--description">{service.description}</p>
-                    </div>
-                    <div className="col-md-6 text-center">
-                        <div className="row">
-                            {service.bg.map(b => {
-                                return (
-                                    <div className="col" key={b}>
-                                        <img src={b} className="service__img" />
-                                    </div>
-                                )}
+                <div className="col-md-6 text-center">
+                    <div className="row">
+                        {service.bg.map(b => {
+                            return (
+                                <div className="col" key={b}>
+                                    <img src={b} className="service__img" />
+                                </div>
                             )}
-                        </div>
+                        )}
                     </div>
                 </div>
             </div>
-        )
-    })
+            <div className="row">
+                { isOpen && <div className="col-md-5 offset-md-1">
+                    <p className="service--description">{service.read}</p>
+                </div> }
+            </div>
+        </div>
+    )
 }
 
 const Testing = () => {
@@ -183,7 +198,7 @@ const Testing = () => {
                     </div>
                 </section>
                 <section>
-                    <Services />
+                    { testingServices.map(service => <ServiceElement service={service} key={service.title} />) }
                 </section>
             </main>
 
